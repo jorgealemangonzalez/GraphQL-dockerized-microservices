d390d5c0c0c954a8a5b892b338850a1e56aeebce
jorge aleman <jorge@subbo.es>
2019-09-22 18:57:59 +0200

    Initial setup GraphQL with Spring

---
+++ src/main/java/com/graphqlexample/bookdetails/BookDetailsServer.java
@@ -0,0 +1,13 @@
+package com.graphqlexample.bookdetails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookDetailsServer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookDetailsServer.class, args);
+	}
+
+}
